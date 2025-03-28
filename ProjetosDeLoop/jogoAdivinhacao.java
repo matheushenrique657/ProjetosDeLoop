ad381e76a8b61d9e2b3a630c95dc9af3c9bae086
matheushenrique657 <matheushenriquenovo871@gmail.com>
2025-03-28 19:27:39 -0300

    Add files via upload

---
+++ ProjetosDeLoop/jogoAdivinhacao.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class jogoAdivinhacao {
+    public static void main(String[] args) {
+        int numeroResposta = (int) (Math.random() * 100) + 1;
+        int numeroInput =101;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adivinhe um número de 1 a 100!");
+        while (numeroInput != numeroResposta) {
+        System.out.println("Palpite: ");
+        numeroInput = sc.nextInt();
+
+        if (numeroInput > numeroResposta) {
+            System.out.print("Tente um número mais baixo: ");
+        } else if (numeroInput < numeroResposta) {
+            System.out.print("Tente um número mais alto: ");
+        }
+        }
+        System.out.println("Numero Correto!");
+    }
+}
